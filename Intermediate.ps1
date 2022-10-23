11590b3839c9de65467d65c0d434efa1de292204
Jérémy Renaud <108636211+jeremyrenaud42@users.noreply.github.com>
2022-10-23 10:39:53 -0400

    Delete Intermediate.ps1

--- Intermediate.ps1
+++
@@ -1,4 +0,0 @@
-$driveletter = $pwd.drive.name #retourne la lettre du disque actuel
-$root = "$driveletter" + ":" #rajoute  : pour que sa fit dans le path
-Set-Location "$root\_Tech\"
-Start-Process "$root\_Tech\menu.exe"
