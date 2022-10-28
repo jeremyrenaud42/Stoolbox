9c163ca954464ffaaf85e5d70c869dcc5f4f35e2
Jérémy Renaud <108636211+jeremyrenaud42@users.noreply.github.com>
2022-10-28 12:45:40 -0400

    delete vide la corbeille

--- delete.ps1
+++ delete.ps1
@@ -1,1 +1,6 @@
-﻿Remove-Item  "C:\_Tech" -Recurse -Force
+﻿write-host "Suppression du dossier c:\_Tech"
+Remove-Item  "C:\_Tech" -Recurse -Force
+Write-Host "Vidage de la corbeille"
+Clear-RecycleBin -DriveLetter 'C' -Force -ErrorAction SilentlyContinue
+Write-Host "La corbeille a été vidé"
+start-sleep -s 2
