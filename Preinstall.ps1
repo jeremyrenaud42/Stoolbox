cd21a7bc48e20c191d41a0af601065842d6fa724
Jérémy Renaud <108636211+jeremyrenaud42@users.noreply.github.com>
2022-10-30 17:34:29 -0400

    améliorer preinstall

--- Preinstall.ps1
+++ Preinstall.ps1
@@ -1,23 +1,53 @@
-﻿$verif = Test-Path "C:\_Tech\menu.ps1"
-if($verif)
+﻿function SourceMenu #Créer dossier et met à jours tout ce qui touche menu, sauf preinstall.ps1 (lui meme)
 {
-    Start-Process powershell.exe "C:\_Tech\Applications\source\scripts\intermediatebat.ps1" -WindowStyle Hidden
-}
-else
-{
-    Write-Host "Le dossier C:\_Tech a été créé"
-    New-Item -ItemType Directory "C:\_Tech" -Force #Créer le dossier _Tech sur le C:
-    write-host "Copie des fichiers sur le C:"
-    Copy-Item "$PSscriptroot\*" "C:\_TECH" -Recurse -Force #copy tous le dossier _Tech de la clé USB vers le dossier _Tech du C:
-    Start-Process powershell.exe "C:\_Tech\Applications\source\scripts\intermediatebat.ps1" -WindowStyle Hidden
+    $Applications = test-path "$Psscriptroot\Applications" 
+    if($Applications -eq $false)
+    {
+        New-Item "$Psscriptroot\Applications" -ItemType Directory -Force | Out-Null
+    }
+
+    $Source = test-path "$Psscriptroot\Applications\Source"  
+    if($Source -eq $false)
+    {
+        New-Item "$Psscriptroot\Applications\Source" -ItemType Directory -Force | Out-Null
+    }
+    
+    $scripts = test-path "$Psscriptroot\Applications\Source\scripts" 
+    if($scripts -eq $false)
+    {
+        New-Item "$Psscriptroot\Applications\Source\scripts" -ItemType Directory -Force | Out-Null
+    }
+    start-sleep -s 2
+    Invoke-WebRequest 'https://raw.githubusercontent.com/jeremyrenaud42/Bat/main/delete.ps1' -OutFile "$Psscriptroot\applications\source\scripts\delete.ps1" | Out-Null 
+    Invoke-WebRequest 'https://raw.githubusercontent.com/jeremyrenaud42/Bat/main/RunAsMenu.bat' -OutFile "$Psscriptroot\applications\source\scripts\RunAsMenu.bat" | Out-Null  
+    Invoke-WebRequest 'https://raw.githubusercontent.com/jeremyrenaud42/Bat/main/Remove.bat' -OutFile "$Psscriptroot\Remove.bat" | Out-Null
+    Invoke-WebRequest 'https://raw.githubusercontent.com/jeremyrenaud42/Bat/main/Menu.bat' -OutFile "$Psscriptroot\Menu.bat" | Out-Null 
+    Invoke-WebRequest 'https://raw.githubusercontent.com/jeremyrenaud42/Bat/main/Menu.ps1' -OutFile "$Psscriptroot\Menu.ps1" | Out-Null     
 }
 
-$intermediatebat = test-path "$root\_Tech\applications\source\scripts\Intermediatebat.ps1"
-if($intermediatebat -eq $false)
+function Launch #Copie tout dans la clé ou lance le script
 {
-    New-Item "$root\_Tech\Applications\Source\scripts" -ItemType Directory -Force | Out-Null 
-    Invoke-WebRequest 'https://raw.githubusercontent.com/jeremyrenaud42/Bat/main/Intermediatebat.ps1' -OutFile "$root\_Tech\applications\source\scripts\Intermediatebat.ps1" | Out-Null
-    Invoke-WebRequest 'https://raw.githubusercontent.com/jeremyrenaud42/Bat/main/delete.ps1' -OutFile "$root\_Tech\applications\source\scripts\delete.ps1" | Out-Null 
-    Invoke-WebRequest 'https://raw.githubusercontent.com/jeremyrenaud42/Bat/main/RunAsMenu.bat' -OutFile "$root\_Tech\applications\source\scripts\RunAsMenu.bat" | Out-Null  
-    Invoke-WebRequest 'https://raw.githubusercontent.com/jeremyrenaud42/Bat/main/Remove.bat' -OutFile "$root\_Tech\Remove.bat" | Out-Null  
+    $menups1 = Test-Path "C:\_Tech\Menu.ps1" 
+    $menubat = Test-Path "C:\_Tech\Applications\Source\scripts\RunAsMenu.bat"
+
+    if($menups1 -and $menubat)
+    {
+        Start-Process "C:\_Tech\Applications\Source\scripts\RunAsMenu.bat" -WindowStyle Hidden
+        exit
+    }
+    else
+    {
+        Write-Host "Le dossier C:\_Tech a été créé"
+        Start-Sleep -s 1
+        New-Item -ItemType Directory "C:\_Tech" -Force | Out-Null #Créer le dossier _Tech sur le C:
+        write-host "Copie des fichiers sur le C:"
+        Start-Sleep -s 1
+        Copy-Item "$Psscriptroot\*" "C:\_TECH" -Recurse -Force | Out-Null #copy tous le dossier _Tech de la clé USB vers le dossier _Tech du C:
+        Start-Sleep -s 1
+        Start-Process "C:\_Tech\Applications\Source\scripts\RunAsMenu.bat" -WindowStyle Hidden
+        exit
+    }
 }
+
+SourceMenu
+Launch
