207db10400aadcd059851c3fd2df80aa7648f82f
Jérémy Renaud <108636211+jeremyrenaud42@users.noreply.github.com>
2022-10-30 18:17:09 -0400

    ajouter verif test-path

--- delete.ps1
+++ delete.ps1
@@ -4,3 +4,9 @@ Write-Host "Vidage de la corbeille"
 Clear-RecycleBin -DriveLetter 'C' -Force -ErrorAction SilentlyContinue
 Write-Host "La corbeille a été vidé"
 start-sleep -s 2
+
+$path = Test-Path "C:\_Tech"
+if($path)
+{
+    write-host  "La suppression du dossier C:\_Tech a échoué"
+}
