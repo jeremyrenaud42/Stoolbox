80fcf6fe07415a4f9be3b053005ac985d50940c4
Jérémy Renaud <108636211+jeremyrenaud42@users.noreply.github.com>
2022-10-30 21:45:28 -0400

    amélioré delete

--- delete.ps1
+++ delete.ps1
@@ -1,12 +1,15 @@
-﻿write-host "Suppression du dossier c:\_Tech"
-Remove-Item  "C:\_Tech" -Recurse -Force
+﻿Add-Type -AssemblyName Microsoft.VisualBasic
+
+write-host "Suppression du dossier c:\_Tech"
+Remove-Item  "C:\_Tech" -Recurse -Force -ErrorAction SilentlyContinue | Out-Null
+start-sleep -s 2
 Write-Host "Vidage de la corbeille"
-Clear-RecycleBin -DriveLetter 'C' -Force -ErrorAction SilentlyContinue
+Clear-RecycleBin -DriveLetter 'C' -Force -ErrorAction SilentlyContinue | Out-Null
 Write-Host "La corbeille a été vidé"
 start-sleep -s 2
 
 $path = Test-Path "C:\_Tech"
 if($path)
 {
-    write-host  "La suppression du dossier C:\_Tech a échoué"
+    [Microsoft.VisualBasic.Interaction]::MsgBox("La suppression du dossier C:\_Tech a échoué",'OKOnly,SystemModal,Information', "Suppression") | Out-Null
 }
