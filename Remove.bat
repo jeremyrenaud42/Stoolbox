861ae836d597538b3d7b631f1708ab983600e8e9
Jérémy Renaud <108636211+jeremyrenaud42@users.noreply.github.com>
2022-12-10 20:32:43 -0500

    remove va chercher delete dans c:\temp

--- Remove.bat
+++ Remove.bat
@@ -1,2 +1,3 @@
 @echo off
-START powershell.exe -executionpolicy unrestricted -command %~d0\_TECH\Applications\source\scripts\delete.ps1 -Verb runAs
+--START powershell.exe -executionpolicy unrestricted -command %~d0\_TECH\Applications\source\scripts\delete.ps1 -Verb runAs
+START powershell.exe -executionpolicy unrestricted -command C:\Temp\delete.ps1 -Verb runAs
