261ae99e4b5d5807893e92b0e09238b2d55ef604
Jérémy Renaud <108636211+jeremyrenaud42@users.noreply.github.com>
2022-12-25 16:30:02 -0500

    menu ne call plus preinstall, mais menu.ps1

--- Menu.bat
+++ Menu.bat
@@ -1,2 +1,3 @@
 @echo off
-START powershell.exe -executionpolicy unrestricted -command %~d0\_TECH\preinstall.ps1 -Verb runAs
+--START powershell.exe -executionpolicy unrestricted -command %~d0\_TECH\preinstall.ps1 -Verb runAs
+START powershell.exe -executionpolicy unrestricted -command %~d0\_TECH\Menu.ps1 -Verb runAs
