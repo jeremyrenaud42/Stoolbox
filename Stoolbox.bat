f35b61cc6e1053577d85bbc108b721dea61703a3
Jérémy Renaud <108636211+jeremyrenaud42@users.noreply.github.com>
2022-12-24 21:13:32 -0500

    ajout verif internet dans le .bat

--- Stoolbox.bat
+++ Stoolbox.bat
@@ -1,4 +1,17 @@
 @echo off
+
+rem verifier internet
+:loop
+ping -n 1 www.google.com >nul
+if %errorlevel% equ 0 (
+  goto end
+) else (
+  echo Veuillez vous connecter à Internet
+  ping -n 5 8.8.8.8 >nul
+  goto loop
+)
+:end
+
 rem créer un dossier nommé "_tech" dans le lecteur C:
 mkdir c:\_Tech 2> nul
 
