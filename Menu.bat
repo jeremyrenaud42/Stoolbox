1fc0b39747ca390750fa402b80f8b5430c038869
Jérémy Renaud <108636211+jeremyrenaud42@users.noreply.github.com>
2022-12-25 16:33:27 -0500

    rajouter le windowstyle hidden

--- Menu.bat
+++ Menu.bat
@@ -1,3 +1,3 @@
 @echo off
 --START powershell.exe -executionpolicy unrestricted -command %~d0\_TECH\preinstall.ps1 -Verb runAs
-START powershell.exe -executionpolicy unrestricted -command %~d0\_TECH\Menu.ps1 -Verb runAs
+START powershell.exe -windowstyle hidden -executionpolicy unrestricted -command %~d0\_TECH\Menu.ps1 -Verb runAs
