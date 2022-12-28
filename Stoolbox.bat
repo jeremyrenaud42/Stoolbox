9300e00ac351a8de0c4af25d56150714d983e441
Jérémy Renaud <108636211+jeremyrenaud42@users.noreply.github.com>
2022-12-27 21:02:46 -0500

    Version final

--- Stoolbox.bat
+++ Stoolbox.bat
@@ -1,5 +1,6 @@
 @echo off
-rem verifier internet
+rem Ce script a pour but de downloader la boite à outils. Il permet de faire une recherche Internet et de télécharger l'app
+rem verifier si internet est connecté
 :loop
 ping -n 1 8.8.8.8 >nul
 if %errorlevel% equ 0 (
@@ -11,10 +12,11 @@ if %errorlevel% equ 0 (
 )
 :end
 
-rem créer un dossier nommé "_tech" dans le lecteur C:
-mkdir c:\_Tech 2> nul
+rem créer un dossier nommé "_tech" dans le lecteur C: afin de pouvoir y downloader le script Powershell de preinstall
+if not exist c:\_Tech mkdir c:\_Tech 2>nul
+powershell -Command "if (!(Test-Path -Path 'C:\_Tech')) { New-Item -ItemType Directory -Path 'C:\_Tech' -Force -ErrorAction SilentlyContinue }"
 
 rem Download le script preinstall.ps1 dans le dossier C:\_TECH
-powershell -WindowStyle Hidden -Command "& { (New-Object System.Net.WebClient).DownloadFile('https://raw.githubusercontent.com/jeremyrenaud42/Bat/main/Preinstall.ps1', 'C:\_tech\Preinstall.ps1')}"
+powershell -NoLogo -NoProfile -ExecutionPolicy Bypass -WindowStyle Hidden -Command "& { (New-Object System.Net.WebClient).DownloadFile('https://raw.githubusercontent.com/jeremyrenaud42/Bat/main/Preinstall.ps1', 'C:\_tech\Preinstall.ps1')}"
 rem Lance le script Preinstall
 START /min /wait powershell.exe -executionpolicy unrestricted -command "C:\_TECH\preinstall.ps1" -Verb runAs
