b9ffbe578de77aad7b138234d1d3ba2195fc90a6
Jérémy Renaud <108636211+jeremyrenaud42@users.noreply.github.com>
2023-01-03 09:31:03 -0500

    Test

--- Preinstall.ps1
+++ Preinstall.ps1
@@ -16,39 +16,51 @@ function SourceMenu
     $Applications = test-path "$Psscriptroot\Applications" 
     if($Applications -eq $false)
     {
-        New-Item "$Psscriptroot\Applications" -ItemType Directory -Force | Out-Null
+        New-Item "$Psscriptroot\Applications" -ItemType 'Directory' -Force | Out-Null
     }
 
     $Source = test-path "$Psscriptroot\Applications\Source"  
     if($Source -eq $false)
     {
-        New-Item "$Psscriptroot\Applications\Source" -ItemType Directory -Force | Out-Null
+        New-Item "$Psscriptroot\Applications\Source" -ItemType 'Directory' -Force | Out-Null
     }
     
     $scripts = test-path "$Psscriptroot\Applications\Source\scripts" 
     if($scripts -eq $false)
     {
-        New-Item "$Psscriptroot\Applications\Source\scripts" -ItemType Directory -Force | Out-Null
+        New-Item "$Psscriptroot\Applications\Source\scripts" -ItemType 'Directory' -Force | Out-Null
     }
 
     $modules = test-path "$Psscriptroot\Applications\Source\modules" 
     if($modules -eq $false)
     {
-        New-Item "$Psscriptroot\Applications\Source\modules" -ItemType Directory -Force | Out-Null
+        New-Item "$Psscriptroot\Applications\Source\modules" -ItemType 'Directory' -Force | Out-Null
     }
     
     $images = test-path "$Psscriptroot\Applications\Source\images" 
     if($images -eq $false)
     {
-        New-Item "$Psscriptroot\Applications\Source\images" -ItemType Directory -Force | Out-Null
+        New-Item "$Psscriptroot\Applications\Source\images" -ItemType 'Directory' -Force | Out-Null
     }
-    start-sleep -s 2
 
     #Download des files
-    Invoke-WebRequest 'https://raw.githubusercontent.com/jeremyrenaud42/Bat/main/delete.ps1' -OutFile "$Psscriptroot\applications\source\scripts\delete.ps1" | Out-Null 
-    Invoke-WebRequest 'https://raw.githubusercontent.com/jeremyrenaud42/Bat/main/Remove.bat' -OutFile "$Psscriptroot\Remove.bat" | Out-Null
-    Invoke-WebRequest 'https://raw.githubusercontent.com/jeremyrenaud42/Bat/main/Menu.bat' -OutFile "$Psscriptroot\Menu.bat" | Out-Null
+    $delete = test-path "$Psscriptroot\applications\source\scripts\delete.ps1"
+    if($delete -eq $false)
+    {
+        Invoke-WebRequest 'https://raw.githubusercontent.com/jeremyrenaud42/Bat/main/delete.ps1' -OutFile "$Psscriptroot\applications\source\scripts\delete.ps1"
+    }
+    $remove = test-path "$Psscriptroot\Remove.bat"
+    if($remove -eq $false)
+    {
+        Invoke-WebRequest 'https://raw.githubusercontent.com/jeremyrenaud42/Bat/main/Remove.bat' -OutFile "$Psscriptroot\Remove.bat" | Out-Null
+    }
+    $menubat = test-path "$Psscriptroot\Menu.bat"
+    if($menubat -eq $false)
+    {
+        Invoke-WebRequest 'https://raw.githubusercontent.com/jeremyrenaud42/Bat/main/Menu.bat' -OutFile "$Psscriptroot\Menu.bat" | Out-Null
+    }
     Invoke-WebRequest 'https://raw.githubusercontent.com/jeremyrenaud42/Bat/main/Menu.ps1' -OutFile "$Psscriptroot\Menu.ps1" | Out-Null     
+    <#
     $a = Test-Path "$Psscriptroot\applications\source\Images\fondpluiesize.gif"
     $b = Test-path  "$Psscriptroot\applications\source\Images\Icone.ico" 
     if($a -and $b -eq $false)
@@ -56,6 +68,7 @@ function SourceMenu
         Invoke-WebRequest 'https://raw.githubusercontent.com/jeremyrenaud42/Menu/main/fondpluiesize.gif' -OutFile "$Psscriptroot\applications\source\Images\fondpluiesize.gif" | Out-Null #Download le fond
         Invoke-WebRequest 'https://raw.githubusercontent.com/jeremyrenaud42/Menu/main/Icone.ico' -OutFile "$Psscriptroot\applications\source\Images\Icone.ico" | Out-Null #Download l'icone
     }
+    #>
     Start-Process "$env:SystemDrive\_Tech\Menu.bat" -WindowStyle Hidden #On va pouvoir juste executer menu, car runasmenu est rendu stoolbox.bat
     exit
 }
@@ -75,7 +88,7 @@ function Launch #Copie tout dans la clé ou lance le script
     {
         Write-Host "Le dossier C:\_Tech a été créé"
         Start-Sleep -s 1
-        New-Item -ItemType Directory "C:\_Tech" -Force | Out-Null #Créer le dossier _Tech sur le C:
+        New-Item -ItemType 'Directory' "C:\_Tech" -Force | Out-Null #Créer le dossier _Tech sur le C:
         write-host "Copie des fichiers sur le C:"
         Start-Sleep -s 1
         Copy-Item "$Psscriptroot\*" "C:\_TECH" -Recurse -Force | Out-Null #copy tous le dossier _Tech de la clé USB vers le dossier _Tech du C:
