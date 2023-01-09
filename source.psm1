39a1dbb4434fbe8d87bdb44ac2439f02821b0ec9
Jérémy Renaud <108636211+jeremyrenaud42@users.noreply.github.com>
2023-01-08 22:12:05 -0500

    Delete source.psm1

--- source.psm1
+++
@@ -1,8 +0,0 @@
-function Sourceexist
-{
-$sourcefolder = Test-path ".\Source" #chemin du dossier source
-    if(!($sourcefolder))
-    {
-        New-Item ".\Source" -ItemType 'Directory' -Force | Out-Null #Créer le dossier source si il n'est pas là
-    }
-}
