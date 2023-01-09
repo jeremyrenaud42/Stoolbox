ff8601b08654cf4a3c735a7f2de3558701e16d11
Jérémy Renaud <108636211+jeremyrenaud42@users.noreply.github.com>
2023-01-08 21:37:04 -0500

    premier ajout du module restore

---
+++ restore.psm1
@@ -0,0 +1,19 @@
+function CreateRestorePoint
+{
+    #REG ADD "HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion\SystemRestore" /V "SystemRestorePointCreationFrequency" /T REG_DWORD /D 0 /F
+    Enable-ComputerRestore -Drive "$env:SystemDrive" 
+    Checkpoint-Computer -Description "STO" -RestorePointType "MODIFY_SETTINGS"
+}
+
+function VerifRestorepoint
+{
+    $listrestorepoint = Get-ComputerRestorePoint | Select-Object Description
+    if($listrestorepoint -match "STO")
+    {
+        [System.Windows.MessageBox]::Show("Point de restauration créé avec succès","Point de restauration",0) | Out-Null
+    }
+    else 
+    {
+        [System.Windows.MessageBox]::Show("Erreur lors de la création du point de restauration","Point de restauration",0) | Out-Null
+    }
+}
