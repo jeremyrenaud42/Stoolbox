9b6c07753a031896667f913e2208f50776f71e1e
Jérémy Renaud <108636211+jeremyrenaud42@users.noreply.github.com>
2023-01-11 21:14:05 -0500

    ajout function remove et copy

--- Modules/Logs.psm1
+++ Modules/Logs.psm1
@@ -1,6 +1,6 @@
 function AddLog ($filename,$message)
 {
-    $logfilepath = ".\Source\$filename"
+    $logfilepath = ".\Source\$filename" #chemin du fichier texte
     (Get-Date).ToString() + " - " + $message + "`r`n" | Out-file -filepath $logfilepath -append -force
 }
 
@@ -8,4 +8,15 @@ function AddErrorsLog ($filename,$message)
 {
     $errorslogfilepath = ".\Source\$filename" #chemin du fichier texte
     (Get-Date).ToString() + " - " + $message + "`r`n" | Out-file -filepath $errorslogfilepath -append -force #ajoute le texte dans le fichier
+}
+
+function RemoveLog($filename)
+{
+    $logfilepath = ".\Source\$filename" #chemin du fichier texte 
+    Remove-Item $logfilepath -Force
+}
+
+function CopyLog($filename,$destination)
+{
+    Copy-Item ".\Source\$filename" -destination $destination -Force
 }
