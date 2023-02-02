e18d529343e611110d1d937c09d0d452800ea1ea
Jérémy Renaud <108636211+jeremyrenaud42@users.noreply.github.com>
2023-02-01 21:01:01 -0500

    Add files via upload

--- Modules/Images.psm1
+++ Modules/Images.psm1
@@ -13,3 +13,12 @@ function DownloadImages
             Invoke-WebRequest 'https://raw.githubusercontent.com/jeremyrenaud42/Menu/main/Icone.ico' -OutFile "$applicationPath\source\Images\Icone.ico" | Out-Null
         } 
 }
+
+function DownloadBackground($app, $lienGithub, $fond)
+{
+    $fondpath = test-Path "$applicationPath\$app\source\$fond"
+        if($fondpath -eq $false) 
+        {
+            Invoke-WebRequest $lienGithub -OutFile "$applicationPath\$app\source\$fond" | Out-Null
+        }
+}
