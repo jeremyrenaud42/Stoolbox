9c85d3f802712431587318f72615dc736edbe44f
Jérémy Renaud <108636211+jeremyrenaud42@users.noreply.github.com>
2023-02-19 20:21:14 -0500

    Delete Folder.psm1

--- Modules/Folder.psm1
+++
@@ -1,9 +0,0 @@
-function CreateFolder($folder) 
-{
-    $folderPath = "$env:SystemDrive\$folder"
-    $folderExist = test-path $folderPath 
-    if($folderExist -eq $false)
-    {
-        New-Item $folderPath -ItemType 'Directory' -Force | Out-Null
-    }
-}
