bb1cb17501327d353f3d4e09f638c495c4c48ece
Jérémy Renaud <108636211+jeremyrenaud42@users.noreply.github.com>
2024-08-22 11:35:59 -0400

    ajout d'une boucle de lockfile

--- Remove.ps1
+++ Remove.ps1
@@ -2,13 +2,29 @@
 
 $desktop = [Environment]::GetFolderPath("Desktop")
 $folderPath = "$env:SystemDrive\_Tech"
+$lockfile = "$env:SystemDrive\_Tech\Applications\source\*lockfile.lock"
+$maxAttempts = 5
+$attempt = 0
 
 if (Test-Path $folderPath)
 {
+    while(Test-Path $lockfile)
+    {
+        Write-Host "En attente de la fermeture des scripts [$attempt/$maxAttempts]"
+        Start-Sleep -s 2
+        $attempt++
+
+        if ($attempt -ge $maxAttempts) 
+        {
+            Write-Host "La suppression de $folderPath va se poursuivre, mais pourrait contenir des erreurs."
+            break
+        }
+    }
     Write-Host "Suppression du dossier $folderPath"
     Remove-Item "$folderPath\*" -Recurse -Force -ErrorAction SilentlyContinue | Out-Null
     Remove-Item $folderPath -Recurse -Force -ErrorAction SilentlyContinue | Out-Null
     Remove-Item "$desktop\Menu.lnk" -Force -ErrorAction SilentlyContinue | Out-Null
+    Remove-Item "$env:SystemDrive\Temp\Remove.bat" -Force -ErrorAction SilentlyContinue | Out-Null
     Start-Sleep -Seconds 2
     Write-Host "Vidage de la corbeille"
     Clear-RecycleBin -Force -ErrorAction SilentlyContinue | Out-Null
