d0e5df26187f2b32d3d66eda303effd0fb50d40f
Jérémy Renaud <108636211+jeremyrenaud42@users.noreply.github.com>
2024-08-28 10:04:31 -0400

    ajout -Principal
    Pour être capable de unregister

--- Modules/Task.psm1
+++ Modules/Task.psm1
@@ -16,7 +16,9 @@ function Invoke-Task
         $action = New-ScheduledTaskAction -Execute $ExecutedScript
         $seconds = '05'
         $trigger = New-ScheduledTaskTrigger -At (Get-Date).AddSeconds($seconds) -Once
+        $currentUser = whoami
+        $principal = New-ScheduledTaskPrincipal -UserId $currentUser -RunLevel Highest
         $settings = New-ScheduledTaskSettingsSet -StartWhenAvailable -AllowStartIfOnBatteries -DontStopIfGoingOnBatteries -MultipleInstances IgnoreNew -Compatibility Win8 #si ordi éteint, le refait après 10 minutes
-        $task = New-ScheduledTask -Action $action -Trigger $trigger -Settings $settings
+        $task = New-ScheduledTask -Action $action -Trigger $trigger -Settings $settings -Principal $principal
         Register-ScheduledTask -TaskName $Taskname -InputObject $task
 }
