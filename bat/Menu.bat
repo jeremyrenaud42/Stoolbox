cf39d82380f21cbfee3a5f81d42508c89937ea92
Jérémy Renaud <108636211+jeremyrenaud42@users.noreply.github.com>
2024-11-09 14:49:12 -0500

    Update Menu.bat

--- bat/Menu.bat
+++ bat/Menu.bat
@@ -1,11 +1,2 @@
 @echo off
-if "%1"=="-v2" (
-    echo Version 2
-    goto :end)
-
-goto :skip
-:end
-START powershell.exe -windowstyle hidden -executionpolicy bypass -command %~d0\_TECH\Menu.ps1 -Verb runAs
-    
-:skip
-pause
+START powershell.exe -windowstyle hidden -executionpolicy unrestricted -command %~d0\_TECH\Menu.ps1 -Verb runAs -arg1 2
