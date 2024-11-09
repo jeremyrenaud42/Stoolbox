503d68bb7ea3214da6ee11893afc09aad0deacb8
Jérémy Renaud <108636211+jeremyrenaud42@users.noreply.github.com>
2024-11-09 14:51:24 -0500

    Update Menu.bat

--- bat/Menu.bat
+++ bat/Menu.bat
@@ -1,2 +1,2 @@
 @echo off
-START powershell.exe -windowstyle hidden -executionpolicy unrestricted -command %~d0\_TECH\Menu.ps1 -Verb runAs -arg1 2
+START powershell.exe -windowstyle hidden -executionpolicy unrestricted -command %~d0\_TECH\Menu.ps1 -Verb runAs
