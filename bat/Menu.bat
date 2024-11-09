b2d730744d74a349e2b6a9e6b1650d98c7b4d320
Jérémy Renaud <108636211+jeremyrenaud42@users.noreply.github.com>
2024-11-09 14:28:28 -0500

    Add files via upload

--- bat/Menu.bat
+++ bat/Menu.bat
@@ -1,2 +1,9 @@
 @echo off
-START powershell.exe -executionpolicy unrestricted -command %~d0\_TECH\Menu.ps1 -Verb runAs
+if "%1"=="-v2" (
+    echo Version 2)
+else
+(
+    START powershell.exe -windowstyle hidden -executionpolicy bypass -command %~d0\_TECH\Menu.ps1 -Verb runAs
+    )
+ 
+pause
